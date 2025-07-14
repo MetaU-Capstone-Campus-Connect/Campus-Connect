3228358fd11cfefe833bf3ee7b42e9f0c34fea9a
Noah Pyrzanowski <noahpyrzanowski@meta.com>
2025-07-14 15:00:45 -0700

    Create Event Fix

--- client/src/components/Events/Events.jsx
+++ client/src/components/Events/Events.jsx
@@ -5,6 +5,7 @@ import Footer from "../Footer";
 import CalendarEvents from "./CalendarEvents";
 import RecommendedEvents from "./RecommendedEvents";
 import MyEvents from "./MyEvents";
+import CreateEvent from './CreateEvent';
 
 function Events({ userName }) {
   const [viewMode, setViewMode] = useState("calendar");
@@ -24,6 +25,7 @@ function Events({ userName }) {
     <div className="Events">
       <Header />
       <div className="eventManager">
+        <CreateEvent userName={userName}/>
         <button onClick={() => setViewMode("calendar")}>Calendar View</button>
 
         <button onClick={() => setViewMode("recommended")}>
